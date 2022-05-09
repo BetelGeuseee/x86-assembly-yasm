f5f46948dac5b9a9b10f5d9d1bb6b27c8782cda8
BetelGeuseee <shirshakupadhayay182@gmail.com>
2022-05-09 22:40:01 +0545

    practised in rough.asm

--- rough.asm
+++ rough.asm
@@ -1,4 +1,24 @@
 section .data
-hello db 95
+listOfNumbers db 10,20,30,40,50,60
+counter db 6
+sum db 0
+average db 0
 
-section 
+section .text
+global _start
+_start:
+  mov bl,listOfNumbers
+  mov cl,byte[counter]
+
+iteration:
+  mov al,[ebx]
+  add byte[sum],al
+  add ebx,1
+  dec cl
+  cmp cl,0 
+  jne iteration
+
+last:
+  mov rax,60
+  mov rdi,0
+  syscall
